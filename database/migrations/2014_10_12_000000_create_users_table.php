1e431a670515f212f1c744333b9e3fdef9e60a37
reyanthony19 <occ.egama.reyanthony@gmail.com>
2025-02-16 11:25:37 +0800

    Egama_ReyAnthony_BSIT3D

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'user','employee'])->default('user');
+            $table->string("address");
+            $table->string("Mobile_Number");
+
             $table->rememberToken();
+            
             $table->timestamps();
         });
     }
